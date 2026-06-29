9be986c050b914e4a4a47506e09e15ac88a56319
Gabriel Coelho <coelhocostag@gmail.com>
2026-06-28 22:29:12 -0300

    feat: add temporary class

---
+++ src/temporary.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "temporary.h"
+
+static unsigned long temporary_counter = 0;
+
+Temporary *temporary_new(void) {
+    Temporary *temporary = (Temporary *)malloc(sizeof(Temporary));
+    if (!temporary) {
+        perror("temporary_new: malloc");
+        exit(1);
+    }
+
+    snprintf(temporary->name, sizeof(temporary->name), "t%lu", temporary_counter++);
+
+    return temporary;
+}
+
+void temporary_free(Temporary *temporary) {
+    if (!temporary) {
+        return;
+    }
+    free(temporary);
+}
+
+const char *temporary_get_name(const Temporary *temporary) {
+    return temporary ? temporary->name : NULL;
+}
---
+++ src/temporary.h
@@ -0,0 +1,12 @@
+#ifndef TEMPORARY_H
+#define TEMPORARY_H
+
+typedef struct Temporary {
+	char name[32];
+} Temporary;
+
+Temporary *temporary_new(void);
+void temporary_free(Temporary *temporary);
+const char *temporary_get_name(const Temporary *temporary);
+
+#endif /* TEMPORARY_H */
