fde6ab3fcd75b2f0963bc3f1c65fd36a8e4343e5
Gabriel Coelho <coelhocostag@gmail.com>
2026-07-01 20:24:30 -0300

    chore: adjust tac-generator comments

--- src/tac-generator.c
+++ src/tac-generator.c
@@ -5,6 +5,8 @@
 
 #include "tac-generator.h"
 
+
+// Função auxiliar para criar uma linha de código TAC formatada
 static char *make_line(const char *format, ...) {
     va_list args;
     va_list copy;
@@ -33,6 +35,7 @@ static char *make_line(const char *format, ...) {
     return buffer;
 }
 
+// gera uma linha TAC com base nos argumentos fornecidos
 char* generate(char *operator, char *argument1, char *argument2, char *result) {
     if (!result) {
         return NULL;
@@ -56,20 +59,24 @@ char* generate(char *operator, char *argument1, char *argument2, char *result) {
                      argument1 ? argument1 : "");
 }
 
+// gera uma label TAC
 char* generate_label(char *label_name) {
     return make_line("%s:\n", label_name ? label_name : "");
 }
 
+// gera um goto TAC
 char* generate_goto(char *label_name) {
     return make_line("goto %s\n", label_name ? label_name : "");
 }
 
+// gera um if-goto TAC
 char* generate_if_goto(char *condition, char *label_name) {
     return make_line("if %s goto %s\n",
                      condition ? condition : "",
                      label_name ? label_name : "");
 }
 
+// gera um cast TAC
 char* generate_cast(char *type_name, char *value, char *result) {
     return make_line("%s = (%s) %s\n",
                      result ? result : "",
@@ -77,6 +84,7 @@ char* generate_cast(char *type_name, char *value, char *result) {
                      value ? value : "");
 }
 
+// gera um return TAC
 char* generate_return(char *expr) {
     if (expr && expr[0] != '\0') {
         return make_line("return %s\n", expr);
@@ -85,22 +93,27 @@ char* generate_return(char *expr) {
     return make_line("return\n");
 }
 
+// gera um return TAC com valor
 char* generate_param(char *arg) {
     return make_line("param %s\n", arg ? arg : "");
 }
 
+// gera um return TAC com valor
 char* generate_print(char *arg) {
     return make_line("print %s\n", arg ? arg : "");
 }
 
+// gera um return TAC instrução de leitura
 char* generate_read(char *arg) {
     return make_line("read %s\n", arg ? arg : "");
 }
 
+// gera um call TAC
 char* generate_call(char *func_name, int num_params) {
     return make_line("call %s, %d\n", func_name ? func_name : "", num_params);
 }
 
+// gera um call TAC com atribuição
 char* generate_call_assign(char *result, char *func_name, int num_params) {
     return make_line("%s = call %s, %d\n",
                      result ? result : "",
--- src/utils.h
+++ src/utils.h
@@ -9,7 +9,6 @@
 #include "tac-generator.h"
 
 int max(int t1, int t2);
-
 char *widen(char *addr, int t1, int t2);
 
 #endif
