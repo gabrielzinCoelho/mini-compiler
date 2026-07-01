f7c9d001317832f809c671a0fd4883972d484305
Heitor Vieira <heitorvieira1220@gmail.com>
2026-07-01 19:57:33 -0300

    feat: add TAC generation

--- src/tac-generator.c
+++ src/tac-generator.c
@@ -1,54 +1,109 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 
 #include "tac-generator.h"
 
+static char *make_line(const char *format, ...) {
+    va_list args;
+    va_list copy;
+    int size;
+    char *buffer;
+
+    va_start(args, format);
+    va_copy(copy, args);
+    size = vsnprintf(NULL, 0, format, copy);
+    va_end(copy);
+
+    if (size < 0) {
+        va_end(args);
+        return NULL;
+    }
+
+    buffer = (char *)malloc((size_t)size + 1);
+    if (!buffer) {
+        va_end(args);
+        return NULL;
+    }
+
+    vsnprintf(buffer, (size_t)size + 1, format, args);
+    va_end(args);
+
+    return buffer;
+}
+
 char* generate(char *operator, char *argument1, char *argument2, char *result) {
-    char buffer[256];
-    snprintf(buffer, sizeof(buffer), "%s, %s, %s, %s",
-           operator ? operator : "",
-           argument1 ? argument1 : "",
-           argument2 ? argument2 : "",
-           result ? result : "");
-    printf("%s\n", buffer);
-    return strdup(buffer);
+    if (!result) {
+        return NULL;
+    }
+
+    if (argument2) {
+        return make_line("%s = %s %s %s\n",
+                         result,
+                         argument1 ? argument1 : "",
+                         operator ? operator : "",
+                         argument2);
+    }
+
+    if (operator && strcmp(operator, "=") == 0) {
+        return make_line("%s = %s\n", result, argument1 ? argument1 : "");
+    }
+
+    return make_line("%s = %s %s\n",
+                     result,
+                     operator ? operator : "",
+                     argument1 ? argument1 : "");
 }
 
 char* generate_label(char *label_name) {
-    char buffer[256];
-    snprintf(buffer, sizeof(buffer), "label %s", label_name ? label_name : "");
-    printf("%s\n", buffer);
-    return strdup(buffer);
+    return make_line("%s:\n", label_name ? label_name : "");
+}
+
+char* generate_goto(char *label_name) {
+    return make_line("goto %s\n", label_name ? label_name : "");
+}
+
+char* generate_if_goto(char *condition, char *label_name) {
+    return make_line("if %s goto %s\n",
+                     condition ? condition : "",
+                     label_name ? label_name : "");
+}
+
+char* generate_cast(char *type_name, char *value, char *result) {
+    return make_line("%s = (%s) %s\n",
+                     result ? result : "",
+                     type_name ? type_name : "",
+                     value ? value : "");
 }
 
 char* generate_return(char *expr) {
-    char buffer[256];
-    snprintf(buffer, sizeof(buffer), "return %s", expr ? expr : "");
-    printf("%s\n", buffer);
-    return strdup(buffer);
+    if (expr && expr[0] != '\0') {
+        return make_line("return %s\n", expr);
+    }
+
+    return make_line("return\n");
 }
 
 char* generate_param(char *arg) {
-    char buffer[256];
-    snprintf(buffer, sizeof(buffer), "param %s", arg ? arg : "");
-    printf("%s\n", buffer);
-    return strdup(buffer);
+    return make_line("param %s\n", arg ? arg : "");
+}
+
+char* generate_print(char *arg) {
+    return make_line("print %s\n", arg ? arg : "");
+}
+
+char* generate_read(char *arg) {
+    return make_line("read %s\n", arg ? arg : "");
 }
 
 char* generate_call(char *func_name, int num_params) {
-    char buffer[256];
-    snprintf(buffer, sizeof(buffer), "call %s, %d", func_name ? func_name : "", num_params);
-    printf("%s\n", buffer);
-    return strdup(buffer);
+    return make_line("call %s, %d\n", func_name ? func_name : "", num_params);
 }
 
 char* generate_call_assign(char *result, char *func_name, int num_params) {
-    char buffer[256];
-    snprintf(buffer, sizeof(buffer), "%s = call %s, %d", 
-           result ? result : "", 
-           func_name ? func_name : "", 
-           num_params);
-    printf("%s\n", buffer);
-    return strdup(buffer);
+    return make_line("%s = call %s, %d\n",
+                     result ? result : "",
+                     func_name ? func_name : "",
+                     num_params);
 }
--- src/tac-generator.h
+++ src/tac-generator.h
@@ -7,8 +7,13 @@ typedef struct TAC {
 
 char* generate(char *operator, char *argument1, char *argument2, char *result);
 char* generate_label(char *label_name);
+char* generate_goto(char *label_name);
+char* generate_if_goto(char *condition, char *label_name);
+char* generate_cast(char *type_name, char *value, char *result);
 char* generate_return(char *expr);
 char* generate_param(char *arg);
+char* generate_print(char *arg);
+char* generate_read(char *arg);
 char* generate_call(char *func_name, int num_params);
 char* generate_call_assign(char *result, char *func_name, int num_params);
 
